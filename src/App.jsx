d932efb71bd3208188b86900871241983d863a53
ArafatSayyed <arafatsayyed07@gmail.com>
2026-08-09 18:53:39 +0530

    Add number input functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,54 @@
+import { useState } from "react";
+
 function App()
 {
-  return (
+  const [display, setDisplay] = useState("0");
+
+  function handleNumber(number)
+  {
+    if (display === "0")
+    {
+      setDisplay(number);
+    }
+    else
+    {
+      setDisplay(display + number);
+    }
+  }
+  return
+  (
     <div className="calculator">
-      <div className="display">0</div>
+      <div className="display">{display}</div>
+
       <div className="buttons">
-        {/* Upper controls */}
         <button className="clear">AC</button>
         <button className="delete">DEL</button>
         <button className="operator">%</button>
         <button className="operator">÷</button>
 
-        {/* Numbers */}
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-
+        <button onClick={() => handleNumber("7")}>7</button>
+        <button onClick={() => handleNumber("8")}>8</button>
+        <button onClick={() => handleNumber("9")}>9</button>
         <button className="operator">×</button>
 
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-
+        <button onClick={() => handleNumber("4")}>4</button>
+        <button onClick={() => handleNumber("5")}>5</button>
+        <button onClick={() => handleNumber("6")}>6</button>
         <button className="operator">−</button>
 
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        
+        <button onClick={() => handleNumber("1")}>1</button>
+        <button onClick={() => handleNumber("2")}>2</button>
+        <button onClick={() => handleNumber("3")}>3</button>
         <button className="operator">+</button>
 
-        {/* Bottom row */}
-        <button className="zero">0</button>
-        <button>•</button>
+        <button className="zero" onClick={() => handleNumber("0")}>
+          0
+        </button>
+        <button>.</button>
         <button className="equals">=</button>
       </div>
     </div>
   );
 }
+
 export default App;
